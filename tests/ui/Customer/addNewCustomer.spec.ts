56b39f3ce1ba59cfc0883c2081989d951f3f3a2d
Orcun FAZLI <30703148+ofazli@users.noreply.github.com>
2026-08-28 22:09:03 -0400

    small fixes
    add clickLogo

--- tests/ui/Customer/addNewCustomer.spec.ts
+++ tests/ui/Customer/addNewCustomer.spec.ts
@@ -3,12 +3,14 @@ import { CustomerFactory } from '../../../factories/CustomerFactory'
 
 test('+Add New Customer Test', async ({
   loginPage,
+  deckPage,
   homePage,
   customerPage,
   newCustomerModalPage,
 }) => {
   const newCustomer = CustomerFactory.create()
   await loginPage.goToMainPage()
+  await deckPage.clickErpNextLogo()
   await homePage.assertErpNextTextMsg('ERPNext')
   await homePage.clickCustomerBtn()
   await customerPage.assertCustomerTxt('Customer')
